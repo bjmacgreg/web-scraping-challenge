e8d406295107d7347557d8a664432868b061a797
Barbara MacGregor <1453-macgregb@users.noreply.umn.bootcampcontent.com>
2020-09-10 00:36:09 -0500

    More

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
-
 app = Flask(__name__)
 
 # Use flask_pymongo to set up mongo connection
@@ -12,6 +11,7 @@ app = Flask(__name__)
 # Or set inline
 mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
+# Define database and collection
 
 @app.route('/')
 def index():
@@ -19,12 +19,12 @@ def index():
     return render_template('index.html', mars=mars)
 
 
-@app.route('/scrape_mars')
+@app.route('/scrape')
 def scrape():
     mars = mongo.db.mars
-    data = scrape_mars.scrape()
+    mars_data = scrape_mars.scrape()
+    mars.update({}, mars_data, upsert=True)
     return redirect("/", code=302)
-
 
 if __name__ == "__main__":
     app.run(debug=True)
--- scrape_mars.py
+++ scrape_mars.py
@@ -1,5 +1,4 @@
-
-import pymongo
+#import pymongo
 from bs4 import BeautifulSoup as bs
 import os
 import requests
@@ -9,90 +8,60 @@ from splinter.exceptions import ElementDoesNotExist
 import urllib.request
 import urllib.parse
 import re
+import shutil
 
 def init_browser():
     executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    return Browser("chrome", usr/local/bin/chromedriver, headless=False)
+    return Browser("chrome", **executable_path, headless=False)
 
 def scrape():
-    # ### NASA Mars News
+    browser = init_browser()
 
-    # Initialize PyMongo to work with MongoDBs
-    conn = 'mongodb://localhost:27017'
-    client = pymongo.MongoClient(conn)
+    #Dictionary to collect data
+    mars = {}
 
-    # Define database and collection
-    db = client.NASA_Mars_news_db
-    stories = db.stories
+    #### Latest NASA Mars News Story####
 
     # URL of page to be scraped
     url = 'https://mars.nasa.gov/news/'
-
     # Retrieve page with the requests module
     response = requests.get(url)
-
     # Create BeautifulSoup object; parse with 'lxml'
     soup = bs(response.text, 'lxml')
-    #print(soup.prettify)
 
-    # Examine the results, then determine element that contains sought info
-    # results are returned as an iterable list
+    # Put together link to top story
+    link = [a['href'] for a in soup.select("a[href*=news]")][0]
+    base = "https://mars.nasa.gov/news"
+    url = urllib.parse.urljoin(base,link)
 
-    results = soup.find_all(class_='slide')
-    #results
-
-    # Loop through returned results
-
-    for result in results:
-        try:
-            news_title = result.find('div', class_="content_title").get_text()
-            news_p = result.find('div', class_="rollover_description_inner").get_text()
-            if (news_title and news_p):
-                print ('-------------')
-                print (news_title)
-                print (news_p)
-            # Dictionary to be inserted as a MongoDB document
-                post = {
-                    'news_title': news_title.strip(),
-                    'news_p': news_p.strip()
-                }
-                stories.insert_one(post)
-                print(post)
-        except Exception as e:
-            print(e)
-            
-    #print(stories)
-
-    NASA_news = db.stories.find()
-
-    #for article in NASA_news:
-    #    print(article)
-
-
-    # ### JPL Mars Space Images - Featured Image
-
-
-    # Thanks to GitHub for the fancybox help, I was really stuck. Didn't realize "fancybox" could be a class. 
-    #https://github.com/ZL14E161110/HW13---Web-Scraping-and-Document-Databases
-    #Didn't look at the rest of it, although I'm sure it's brilliant. 
-
-    executable_path = {'executable_path': '/usr/local/bin/chromedriver'}
-    browser = Browser('chrome', **executable_path, headless=False)
-    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    # Fetch headline and text  
     browser.visit(url)
-    html = browser.html
-    soup = bs(html, 'lxml')
+    news_title = browser.find_by_tag('h1').value
+    news_p = browser.find_by_id('primary_column').value
+    news_p = news_p.replace('\n', ' ')
+    #tried a million ways to get rid of remaining \ (see old notebook), no luck yet
 
-    #Assuming the latest Mars image will always be the one in the first position of the grid...
-    #Note 0 is the overall featured image, which is not necessarily of Mars.
+    # Add title and text to data dictionary 
+    mars["news_title"] = news_title
+    mars["news_p"] = news_p
 
+    #### JPL Mars Space Images - Featured Image ####
+ 
+    # Visit site and make soup
+    url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    response = requests.get(url)
+    soup = bs(response.text, 'lxml')
+
+    #Get image 
     partial_address = soup.find_all('a', class_='fancybox')[1].get('data-fancybox-href').strip()
     base = "https://www.jpl.nasa.gov"
     featured_image_url = urllib.parse.urljoin(base, partial_address)
-    #featured_image_url
-    browser.quit()
 
-    # ### Mars Facts
+    #Add url to dictionary
+    mars["featured_image_url"] = featured_image_url    
+    
+    
+    #### Mars Facts
 
     url = 'https://space-facts.com/mars/'
 
@@ -103,40 +72,33 @@ def scrape():
 
     mars_facts_df = tables[0]
     mars_facts_df.set_index([0], inplace=True)
-    #mars_facts_df
 
     mars_facts_df.rename(columns=mars_facts_df.iloc[0]).drop(mars_facts_df.index[0])
-    #mars_facts_df
 
     mars_facts_html_table = mars_facts_df.to_html()
     mars_facts_html_table = mars_facts_html_table.replace('\n', '')
-    #mars_facts_html_table
-    #print(mars_facts_html_table)
+
+    #Add table to dictionary
+    mars["table"] = mars_facts_html_table
 
     ###Make sure extra header row is gone###
 
 
-    # ### Mars Hemispheres
+    #### Mars Hemispheres
 
-    #navigate to starting page
-    executable_path = {"executable_path": "/usr/local/bin/chromedriver"}
-    browser = Browser("chrome", **executable_path, headless=False)
     url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
     browser.visit(url)
 
-    #Make soup
     # Retrieve page with the requests module
     response = requests.get(url)
 
     # Create BeautifulSoup object; parse with 'lxml'
     soup = bs(response.text, 'lxml')
-    #print(soup.prettify)
 
     #Find titles and links for full-scale images
 
-    title_list = []
-    img_url_list = []
     base = "https://astrogeology.usgs.gov"
+    hemisphere_image_urls = []
 
     for link in soup.find_all(class_='itemLink product-item'):
         partial = link.get('href')    
@@ -152,11 +114,13 @@ def scrape():
         soup = bs(html, 'lxml')
         title = soup.find(class_ = 'title').get_text()
         img_url = [a['href'] for a in soup.select('a:contains(Original)')]
-        title_list.append(title)
-        img_url_list.append(*img_url)
-    #    print(title, *img_url)
+        img_url = str(*img_url)
+        hemisphere_image_urls.append({'title':title, 'img_url':img_url})
     browser.quit()
-    hemisphere_image_urls = zip(title_list, img_url_list)
+
+    mars["hemisphere_image_urls"] = hemisphere_image_urls
+
+    return mars
 
 
 
